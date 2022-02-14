47ab4745be107fc1f0758ad459a7cec0104a1cf2
bibeauv <valerie.bibeau@polymtl.ca>
2022-02-14 13:14:53 -0500

    sim100k

--- sim100k/launch_folders.sh
+++ sim100k/launch_folders.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --account=rrg-blaisbru
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=10G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=valerie.bibeau@hotmail.ca
 #SBATCH --output=%x-%j.out
 
-source $SCRATCH/.dealii
-module load python/3
-module load scipy-stack
 source $SCRATCH/ENV/bin/activate
 srun python3 launch_folders.py
--- sim100k/launch_lethe.py
+++ sim100k/launch_lethe.py
@@ -3,8 +3,8 @@ import sys
 sys.path.insert(1, '/home/bibeauv/scratch/sim100k')
 import MixerSim as MS
 
-MS.launch_gmsh('/home/bibeauv/scratch/soft/gmsh-4.5.6-Linux64/bin/gmsh',min_mesh_length=0.003,min_max=10,decrease=0.001)
+MS.launch_gmsh('gmsh',min_mesh_length=0.003,min_max=10,decrease=0.001)
 
-lethe = '/home/bibeauv/scratch/soft/lethe/build/applications/gls_navier_stokes_3d/gls_navier_stokes_3d'
+lethe = '/home/bibeauv/scratch/soft/lethe/inst/bin/gls_navier_stokes_3d'
 
 os.system(lethe + ' mixer.prm')
